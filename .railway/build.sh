a8660853a0da3ab98764033253676361ae4cab3b
Royal_Code_Plague <robotix100music@gmail.com>
2026-04-18 01:27:07 +0200

    Simplify Railway config: remove cached nixpacks, use inline commands

--- .railway/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd backend
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-python manage.py collectstatic --noinput
--- .railway/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd backend
-python manage.py migrate
-python manage.py runserver 0.0.0.0:$PORT
